b54fd459ae38965e6808902b7b8c3121508ad115
zhou <pangjz001@126.com>
2022-04-01 17:37:28 +0800

    P20完  第二次commit

--- cloud-consumer-order80/src/main/java/com/jbf/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/jbf/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.jbf.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
  * @version: 1.0
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
--- cloud-eureka-server7001/src/main/java/com/jbf/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/jbf/springcloud/EurekaMain7001.java
@@ -2,18 +2,18 @@ package com.jbf.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Auther: zhou
- * @Date: 2022/4/1 - 04 - 01 - 11:06
- * @Description: PACKAGE_NAME
+ * @Date: 2022/4/1 - 04 - 01 - 16:35
+ * @Description: com.jbf.springcloud
  * @version: 1.0
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
